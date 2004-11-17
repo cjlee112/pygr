c5f9df16c51907a722fc56726ec4f750e2a01bf1
Christopher Lee <leec@chem.ucla.edu>
2004-11-17 22:46:09 +0000

    rename blast function as blast()

--- blasttest.py
+++ blasttest.py
@@ -2,7 +2,7 @@
 from seqdb import *
 db=BlastDB('sp') # OPEN SWISSPROT BLAST DB
 s=NamedSequence(str(db['CYGB_HUMAN'][40:-40]),'boo')
-m=db*s # DO BLAST SEARCH
+m=db.blast(s) # DO BLAST SEARCH
 myg=db['MYG_CHICK']
 for i in m[s][myg]:
     print repr(i.srcPath),repr(i.destPath),i.blast_score,i.percent_id
