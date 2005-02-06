df7e110554ab6c8f7ba2ee4c6de88acc63628966
Christopher Lee <leec@chem.ucla.edu>
2005-02-06 03:24:28 +0000

    use the new GraphQuery class

--- tests/gqtest.py
+++ tests/gqtest.py
@@ -4,5 +4,5 @@ from graphquery import *
 dataGraph={'a':{'b':None,'c':None,'d':None},'b':{'c':None},'c':{},'d':{}}
 queryGraph={1:{2:None,3:None},2:{3:None},3:{}}
 
-for d in graphquery(dataGraph,queryGraph):
+for d in GraphQuery(dataGraph,queryGraph):
     print 'match:',d
