95c260f18bd4f3b66d15abee44fd8ab0546fad07
Christopher Lee <leec@chem.ucla.edu>
2005-11-04 07:59:13 +0000

    added sequence truncation tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -343,7 +343,7 @@ class TestFrameWork(TestMain):
      class SequenceSuite(unittest.TestCase):
         'basic sequence class tests'
         def setUp(self):
-           self.seq=sequence.NamedSequence('atttgactatgctccag','foo')
+           self.seq=sequence.Sequence('atttgactatgctccag','foo')
            
         def testLength(self):
            self.assertEqual(len(self.seq),17)
@@ -353,15 +353,36 @@ class TestFrameWork(TestMain):
            self.assertEqual(str(-(self.seq[5:10])),'atagt')
         def testRCSlice(self):
            self.assertEqual(str((-self.seq)[5:10]),'gcata')
+        def testTruncate(self):
+           self.assertEqual(str(self.seq[-202020202:5]),'atttg')
+           self.assertEqual(self.seq[-202020202:5],self.seq[0:5])
+           self.assertEqual(self.seq[-2020202:],self.seq)
+           self.assertEqual(str(self.seq[-202020202:-5]),'atttgactatgc')
+           self.assertEqual(str(self.seq[-5:2029]),'tccag')
+           self.assertEqual(str(self.seq[-5:]),'tccag')
+           self.assertRaises(IndexError,lambda x:x[999:10000],self.seq)
+           self.assertRaises(IndexError,lambda x:x[-10000:-3000],self.seq)
+           self.assertRaises(IndexError,lambda x:x[1000:],self.seq)
+        def testRCTruncate(self):
+           seq= -self.seq
+           self.assertEqual(str(seq[-202020202:5]),'ctgga')
+           self.assertEqual(seq[-202020202:5],seq[0:5])
+           self.assertEqual(seq[-2020202:],seq)
+           self.assertEqual(str(seq[-202020202:-5]),'ctggagcatagt')
+           self.assertEqual(str(seq[-5:2029]),'caaat')
+           self.assertEqual(str(seq[-5:]),'caaat')
+           self.assertRaises(IndexError,lambda x:x[999:10000],seq)
+           self.assertRaises(IndexError,lambda x:x[-10000:-3000],seq)
+           self.assertRaises(IndexError,lambda x:x[1000:],seq)
         def testjoin(self):
            self.assertEqual(str(self.seq[5:15]*self.seq[8:]),'atgctcc')
         def testRCjoin(self):
            self.assertEqual(str((-(self.seq[5:10]))*((-self.seq)[5:10])),'ata')
         def testseqtype(self):
            self.assertEqual(self.seq.seqtype(),sequence.DNA_SEQTYPE)
-           self.assertEqual(sequence.NamedSequence('auuugacuaugcuccag','foo').seqtype(),
+           self.assertEqual(sequence.Sequence('auuugacuaugcuccag','foo').seqtype(),
                             sequence.RNA_SEQTYPE)
-           self.assertEqual(sequence.NamedSequence('kqwestvvarphal','foo').seqtype(),
+           self.assertEqual(sequence.Sequence('kqwestvvarphal','foo').seqtype(),
                             sequence.PROTEIN_SEQTYPE)
 
      suite_list =[QuerySuite,MappingSuite,IteratorSuite,SequenceSuite]
