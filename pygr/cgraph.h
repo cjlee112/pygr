1dcdb53dd31518582910908bdfb8885e8f76be1d
Christopher Lee <leec@chem.ucla.edu>
2005-11-18 04:01:52 +0000

    switched to using C qsort instead of Python sort()

--- pygr/cgraph.h
+++ pygr/cgraph.h
@@ -23,6 +23,7 @@ typedef struct {
 
 extern CDict *cdict_alloc(int n);
 extern int cdict_free(CDict *d);
+extern int cdict_qsort_cmp(const void *void_a,const void *void_b);
 extern CDictEntry *cdict_getitem(CDict *d,int k);
 extern CGraph *cgraph_alloc(int n);
 extern int cgraph_free(CGraph *d);
