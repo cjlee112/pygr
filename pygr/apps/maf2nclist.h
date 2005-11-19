afd6b38124846f94f3032954802497388fe679ca
Christopher Lee <leec@chem.ucla.edu>
2005-11-19 04:22:29 +0000

    added support for bounds checking in readMAFrecord()

--- pygr/apps/maf2nclist.h
+++ pygr/apps/maf2nclist.h
@@ -5,7 +5,7 @@ typedef struct {
 } SeqNameID_T;
 
 extern int readMAFrecord(IntervalMap im[],int n,SeqNameID_T seqnames[],int nseq0,int *p_nseq1,
-		  int lpoStart,FILE *ifile)
+		  int lpoStart,int *block_len,FILE *ifile,int maxseq)
      ;
 
 extern int seqnameID_qsort_cmp(const void *void_a,const void *void_b)
