c9e494fd4e7417f7bf4905d231f7afd26dc791c5
Christopher Lee <leec@chem.ucla.edu>
2007-07-12 06:48:57 +0000

    added XMLRPC_Test

--- tests/pygrdata_test.py
+++ tests/pygrdata_test.py
@@ -23,12 +23,14 @@ class Seq_Test(object):
         correct = pygrData.Bio.Seq.sp()['HBB1_TORMA'][10:35]
         assert frag == correct, 'seq ival should match'
         assert frag.__doc__ == 'fragment', 'docstring should match'
+        assert str(frag) == 'IQHIWSNVNVVEITAKALERVFYVY', 'letters should match'
         store = PygrDataTextFile('results/seqdb1.pickle')
         saved = store['hbb1 fragment']
         assert frag == saved, 'seq ival should matched stored result'
     def dir_test(self):
         pygrData = self.tempdir.force_reload()
         l = pygrData.dir('Bio')
+        print 'dir:',l
         assert l == ['Bio.Seq.frag','Bio.Seq.sp']
         
 
@@ -47,3 +49,23 @@ class Seq_SQL_Test(Seq_Test):
 class Seq_SQL2_Test(Seq_SQL_Test):
     'test arg passing mechanism to save to a specific database'
     mysqlArgs = dict(args=' lldb.mbi.ucla.edu')
+
+class XMLRPC_Test(object):
+    'create an XMLRPC server and access seqdb from it'
+    @skip_errors(KeyError)
+    def setup(self):
+        self.server = TestXMLRPCServer('Bio.Seq.Swissprot.sp42')
+    def match_test(self):
+        pygrData = self.server.access_server()
+        l = pygrData.dir('Bio')
+        print 'remote dir:',l
+        assert l == ['Bio.Seq.Swissprot.sp42'], 'get resource list by XMLRPC'
+        sp = pygrData.Bio.Seq.Swissprot.sp42()
+        hbb = sp['HBB1_TORMA']
+        assert len(hbb) == 142, 'get length by XMLRPC'
+        ival = hbb[10:35]
+        assert str(ival) == 'IQHIWSNVNVVEITAKALERVFYVY', 'get seq by XMLRPC'
+    def teardown(self):
+        'halt the test XMLRPC server'
+        self.server.close()
+
