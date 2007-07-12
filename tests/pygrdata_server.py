a575926713736f2ea9342e9113b7c483cac82f88
Christopher Lee <leec@chem.ucla.edu>
2007-07-12 06:50:09 +0000

    runs an XMLRPC server on the specified port and serves the specified list of pygr.Data resources

---
+++ tests/pygrdata_server.py
@@ -0,0 +1,22 @@
+
+import sys
+
+def serve_forever(self):
+    self.keepRunning = True
+    while self.keepRunning:
+        self.handle_request()
+
+def exit_now(self):
+    self.keepRunning = False
+    return 0
+
+port = int(sys.argv[1]) # GET THE PORT NUMBER TO USE
+# LOAD THE SPECIFIED RESOURCES FROM PYGR.DATA
+import pygr.Data
+l = [pygr.Data.getResource(name) for name in sys.argv[2:]]
+# CREATE A NEW SERVER THAT WILL SERVE THE RESOURCES WE JUST LOADED
+server=pygr.Data.getResource.newServer('testy',withIndex=True,host='localhost',port=port)
+import new
+m = new.instancemethod(exit_now,server.server,server.server.__class__)
+server.server.register_function(m) # PROVIDE A WAY TO FORCE SERVER TO EXIT
+serve_forever(server.server) # STARTS THE SERVER AND NEVER RETURNS...
