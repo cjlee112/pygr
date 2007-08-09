60f3f0cce2187e6c4c75c63481a87c3593907911
Christopher Lee <leec@chem.ucla.edu>
2007-08-09 06:10:54 +0000

    converted to nose from unittest standard, to make it compatible with protest

--- tests/graph_test.py
+++ tests/graph_test.py
@@ -1,9 +1,9 @@
-import unittest
+
 from nosebase import skip_errors
 from pygr import mapping,graphquery
 
 
-class QuerySuite(unittest.TestCase):
+class Query_Test(object):
     "Pygr Query tests..."
     def dqcmp(self,datagraph,querygraph,result):
         try:
@@ -15,40 +15,40 @@ class QuerySuite(unittest.TestCase):
             datagraph = g
         result_kounter = 0
         l = [d.copy() for d in graphquery.GraphQuery(datagraph,querygraph)]
-        self.failUnless(len(l)==len(result),'length mismatch')
+        assert len(l)==len(result),'length mismatch'
         l.sort()
         result.sort()
         for i in range(len(l)):
-            self.failUnless(l[i]==result[i],'incorrect result')
-    def testBasicQuery(self):  # Basic Query Test
+            assert l[i]==result[i],'incorrect result'
+    def basicquery_test(self):  # Basic Query Test
         datagraph = {0: {1: None, 2: None, 3: None}, 1: {2:None}, 3: {4: None, 5: None},
                      4: {6: None}, 5: {6: None}, 2: {}, 6: {}}
         querygraph = {0: {1: None, 2: None, 3: None}, 3:{4: None},1:{},2:{},4:{}}
         result = [{0: 0, 1: 1, 2: 2, 3: 3, 4: 4}, {0: 0, 1: 1, 2: 2, 3: 3, 4: 5},
                   {0: 0, 1: 2, 2: 1, 3: 3, 4: 4}, {0: 0, 1: 2, 2: 1, 3: 3, 4: 5}]
         self.dqcmp(datagraph,querygraph,result) 
-    def testCyclicQuery(self): # Test cyclic QG against cyclic DG 
+    def cyclicquery_test(self): # Test cyclic QG against cyclic DG 
         datagraph = {1:{2:None},2:{3:None},3:{4:None},4:{5:None},5:{2:None}}
         querygraph = {0:{1:None},1:{2:None},2:{4:None},3:{1:None},4:{3:None}}
         result = [{0: 1, 1: 2, 2: 3, 3: 5, 4: 4}]
         self.dqcmp(datagraph,querygraph,result)
-    def testCyclicAcyclicQuery(self): # Test cyclic QG against acyclic DG 
+    def cyclicacyclicquery_test(self): # Test cyclic QG against acyclic DG 
         datagraph = {0:{1:None},1:{3:None},5:{3:None},4:{5:None},2:{4:None,1:None},3:{}}
         querygraph = {0:{1:None},1:{3:None},3:{5:None},5:{4:None},4:{2:None},2:{1:None}}
         result = []
         self.dqcmp(datagraph,querygraph,result)
-    def testSymmetricQuery(self): # Test symmetrical QG against symmetrical DG
+    def symmetricquery_test(self): # Test symmetrical QG against symmetrical DG
         datagraph = {1:{2:None},2:{3:None,4:None},5:{2:None},3:{},4:{}}
         querygraph = {0:{1:None},1:{2:None},2:{}}
         result = [{0: 1, 1: 2, 2: 3}, {0: 1, 1: 2, 2: 4},
                   {0: 5, 1: 2, 2: 3}, {0: 5, 1: 2, 2: 4}]
         self.dqcmp(datagraph,querygraph,result)
-    def testFilteredQuery(self): # Test a filter against a query
+    def filteredquery_test(self): # Test a filter against a query
         datagraph = {0: {1: None, 2: None, 3: None}, 1: {2: None, 3: None}, 3: {4: None}}
         querygraph = {0:{1:{'filter':lambda toNode,**kw:toNode == 3}},1:{}}
         result = [{0: 0, 1: 3},{0: 1, 1: 3}]
         self.dqcmp(datagraph,querygraph,result)
-    def testHeadlessQuery(self): # Test a query with no head nodes
+    def headlessquery_test(self): # Test a query with no head nodes
         datagraph = {0:{1:None},1:{2:None},2:{3:None},3:{4:None},4:{1:None}}
         querygraph = {0:{1:None},1:{2:None},2:{3:None},3:{0:None}}
         result = [{0: 1, 1: 2, 2: 3, 3: 4},
@@ -57,44 +57,43 @@ class QuerySuite(unittest.TestCase):
                   {0: 4, 1: 1, 2: 2, 3: 3}]
         self.dqcmp(datagraph,querygraph,result)
 
-class MappingSuite(QuerySuite):
-    def setUp(self):
+class Mapping_Test(Query_Test):
+    def setup(self):
         self.datagraph = mapping.dictGraph()
-    def testGraphDict(self):
+    def graphdict_test(self):
         datagraph = self.datagraph
         datagraph += 1 
         datagraph[1] += 2
         results = {1: {2: None}, 2: {}}
-        self.failUnless(datagraph == results, 'incorrect result')
-    def testNodeDel(self): 
+        assert datagraph == results, 'incorrect result'
+    def nodedel_test(self): 
         datagraph = self.datagraph
         datagraph += 1
         datagraph += 2 
         datagraph[2] += 3
         datagraph -= 1
         results = {2: {3: None}, 3: {}}
-        self.failUnless(datagraph == results, 'incorrect result')     
-    def testDelRaise(self):
+        assert datagraph == results, 'incorrect result'
+    def delraise_test(self):
         datagraph = self.datagraph
         datagraph += 1
         datagraph += 2
         datagraph[2] += 3
-        error = 0
         try:
             for i in range(0,2):
                 datagraph -= 3
+            raise ValueError('failed to catch bad node deletion attempt')
         except KeyError:
-            error = 1 
-        self.failUnless(error,'incorrect result')
-    def testSetItemRaise(self):
+            pass # THIS IS THE CORRECT RESULT
+    def setitemraise_test(self):
         datagraph = self.datagraph
         datagraph += 1
         try:
             datagraph[1] = 2
+            raise KeyError('failed to catch bad setitem attempt')
         except ValueError:
-            error = 1
-        self.failUnless(error,'incorrect result')
-    def testGraphEdges(self): 
+            pass # THIS IS THE CORRECT RESULT
+    def graphedges_test(self): 
         datagraph = self.datagraph
         graphvals = {1:{2:None},2:{3:None,4:None},5:{2:None},3:{},4:{}}
         edge_list = [[1, 2,None], [2, 3,None], [2, 4,None], [5, 2,None]]
@@ -109,29 +108,29 @@ class MappingSuite(QuerySuite):
         edge_results = [list(t) for t in edge_results]
         edge_list.sort()
         print 'edge_results:',edge_results
-        self.failUnless(edge_results == edge_list, 'incorrect result')        
+        assert edge_results == edge_list, 'incorrect result'        
 
 
-class GraphSuite(MappingSuite):
+class Graph_Test(Mapping_Test):
     'run same tests on mapping.Graph class'
-    def setUp(self):
+    def setup(self):
         self.datagraph = mapping.Graph()
 
-class GraphShelveSuite(MappingSuite):
+class GraphShelve_Test(Mapping_Test):
     'run same tests on mapping.Graph class'
-    def setUp(self):
+    def setup(self):
         from nosebase import TempDir
         tmp = TempDir()
         self.datagraph = mapping.Graph(filename=tmp.subfile('mygraph'),
                                        intKeys=True)
         self.tempdir = tmp # KEEP BOUND SO NOT DELETED UNTIL THIS TEST COMPLETED
-    def tearDown(self):
+    def teardown(self):
         self.tempdir.__del__() # FORCE IT TO DELETE TEMPORARY DIRECTORY
 
-class SQLGraphSuite(MappingSuite):
+class SQLGraph_Test(Mapping_Test):
     'run same tests on mapping.SQLGraph class'
     @skip_errors(ImportError)
-    def setUp(self):
+    def setup(self):
         from pygr import sqlgraph
         import MySQLdb # TEST WILL BE SKIPPED IF UNAVAILABLE
         try:
@@ -141,6 +140,19 @@ class SQLGraphSuite(MappingSuite):
                                                                 edge_id='int'))
         except MySQLdb.MySQLError:
             raise ImportError # NO SERVER, DATABASE OR PRIVILEGES? SKIP TESTS.
-    def tearDown(self):
+    def teardown(self):
         self.datagraph.cursor.execute('drop table test.dumbo_foo_test')
 
+
+class Splicegraph_Test(object):
+    @skip_errors(KeyError)
+    def setup(self):
+        import pygr.Data
+        self.sg = pygr.Data.Bio.Annotation.ASAP2.Isoform.HUMAN.hg17.splicegraph()
+    def exonskip_megatest(self):
+        'perform exon skip query'
+        from pygr import graphquery
+        query = {0:{1:None,2:None},1:{2:None},2:{}}
+        gq = graphquery.GraphQuery(self.sg,query)
+        l = list(gq)
+        assert len(l)==11546,'test exact size of exonskip set'
