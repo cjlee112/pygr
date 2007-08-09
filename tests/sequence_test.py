48c93eded1a2d805bdc85f067ddc4178dddf11b2
Christopher Lee <leec@chem.ucla.edu>
2007-08-09 06:11:58 +0000

    converted from unittest to nose standard, to make it compatible with protest

--- tests/sequence_test.py
+++ tests/sequence_test.py
@@ -1,59 +1,85 @@
-import unittest
 from pygr import sequence
 from nosebase import *
 
-class SequenceSuite(unittest.TestCase):
+class Sequence_Test(object):
     'basic sequence class tests'
-    def setUp(self):
+    def setup(self):
         self.seq=sequence.Sequence('atttgactatgctccag','foo')     
-    def testLength(self):
-        self.assertEqual(len(self.seq),17)
-    def testSlice(self):
-        self.assertEqual(str(self.seq[5:10]),'actat')
-    def testSliceRC(self):
-        self.assertEqual(str(-(self.seq[5:10])),'atagt')
-    def testRCSlice(self):
-        self.assertEqual(str((-self.seq)[5:10]),'gcata')
-    def testTruncate(self):
-        self.assertEqual(str(self.seq[-202020202:5]),'atttg')
-        self.assertEqual(self.seq[-202020202:5],self.seq[0:5])
-        self.assertEqual(self.seq[-2020202:],self.seq)
-        self.assertEqual(str(self.seq[-202020202:-5]),'atttgactatgc')
-        self.assertEqual(str(self.seq[-5:2029]),'tccag')
-        self.assertEqual(str(self.seq[-5:]),'tccag')
-        self.assertRaises(IndexError,lambda x:x[999:10000],self.seq)
-        self.assertRaises(IndexError,lambda x:x[-10000:-3000],self.seq)
-        self.assertRaises(IndexError,lambda x:x[1000:],self.seq)
-    def testRCTruncate(self):
+    def length_test(self):
+        assert len(self.seq)==17
+    def slice_test(self):
+        assert str(self.seq[5:10])=='actat'
+    def slicerc_test(self):
+        assert str(-(self.seq[5:10]))=='atagt'
+    def rcslice_test(self):
+        assert str((-self.seq)[5:10])=='gcata'
+    def truncate_test(self):
+        assert str(self.seq[-202020202:5])=='atttg'
+        assert self.seq[-202020202:5]==self.seq[0:5]
+        assert self.seq[-2020202:]==self.seq
+        assert str(self.seq[-202020202:-5])=='atttgactatgc'
+        assert str(self.seq[-5:2029])=='tccag'
+        assert str(self.seq[-5:])=='tccag'
+        try:
+            self.seq[999:10000]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+        try:
+            self.seq[-10000:-3000]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+        try:
+            self.seq[1000:]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+    def rctruncate_test(self):
         seq= -self.seq
-        self.assertEqual(str(seq[-202020202:5]),'ctgga')
-        self.assertEqual(seq[-202020202:5],seq[0:5])
-        self.assertEqual(seq[-2020202:],seq)
-        self.assertEqual(str(seq[-202020202:-5]),'ctggagcatagt')
-        self.assertEqual(str(seq[-5:2029]),'caaat')
-        self.assertEqual(str(seq[-5:]),'caaat')
-        self.assertRaises(IndexError,lambda x:x[999:10000],seq)
-        self.assertRaises(IndexError,lambda x:x[-10000:-3000],seq)
-        self.assertRaises(IndexError,lambda x:x[1000:],seq)
-    def testjoin(self):
-        self.assertEqual(str(self.seq[5:15]*self.seq[8:]),'atgctcc')
-    def testRCjoin(self):
-        self.assertEqual(str((-(self.seq[5:10]))*((-self.seq)[5:10])),'ata')
-    def testseqtype(self):
-        self.assertEqual(self.seq.seqtype(),sequence.DNA_SEQTYPE)
-        self.assertEqual(sequence.Sequence('auuugacuaugcuccag','foo').seqtype(),
-                         sequence.RNA_SEQTYPE)
-        self.assertEqual(sequence.Sequence('kqwestvvarphal','foo').seqtype(),
-                         sequence.PROTEIN_SEQTYPE)
+        assert str(seq[-202020202:5])=='ctgga'
+        assert seq[-202020202:5]==seq[0:5]
+        assert seq[-2020202:]==seq
+        assert str(seq[-202020202:-5])=='ctggagcatagt'
+        assert str(seq[-5:2029])=='caaat'
+        assert str(seq[-5:])=='caaat'
+        try:
+            seq[999:10000]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+        try:
+            seq[-10000:-3000]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+        try:
+            seq[1000:]
+            raise ValueError('failed to trap out of bounds slice')
+        except IndexError:
+            pass
+    def join_test(self):
+        assert str(self.seq[5:15]*self.seq[8:])=='atgctcc'
+    def rcjoin_test(self):
+        assert str((-(self.seq[5:10]))*((-self.seq)[5:10]))=='ata'
+    def seqtype_test(self):
+        assert self.seq.seqtype()==sequence.DNA_SEQTYPE
+        assert sequence.Sequence('auuugacuaugcuccag','foo').seqtype() == \
+                         sequence.RNA_SEQTYPE
+        assert sequence.Sequence('kqwestvvarphal','foo').seqtype() == \
+                         sequence.PROTEIN_SEQTYPE
 
 from pygrdata_test import PygrSwissprotBase
 class Blast_Test(PygrSwissprotBase):
+    'test basic blast functionality'
     @skip_errors(OSError,KeyError)
     def setup(self):
         PygrSwissprotBase.setup(self)
         import pygr.Data
         self.sp = pygr.Data.Bio.Seq.Swissprot.sp42()
-        self.sp.formatdb()
+        import os
+        blastIndexPath = os.path.join(os.path.dirname(self.sp.filepath),'wikiwacky')
+        self.sp.formatdb(blastIndexPath)
     def blast_test(self):
         hbb = self.sp['HBB1_TORMA']
         hits = self.sp.blast(hbb)
@@ -74,3 +100,18 @@ class Blast_Test(PygrSwissprotBase):
             raise ValueError('failed to catch bad alignment query')
         except KeyError:
             pass
+
+
+class Blast_reindex_untest(Blast_Test):
+    'test building blast indexes under a different name'
+    @skip_errors(OSError,KeyError)
+    def setup(self):
+        PygrSwissprotBase.setup(self)
+        import pygr.Data
+        self.sp = pygr.Data.Bio.Seq.Swissprot.sp42()
+        import os
+        blastIndexPath = os.path.join(os.path.dirname(self.sp.filepath),'wikiwacky')
+        self.sp.formatdb()
+        #self.sp.formatdb(blastIndexPath) # FORCE IT TO STORE INDEX WITH DIFFERENT NAME
+        #print 'blastIndexPath is',self.sp.blastIndexPath
+        
