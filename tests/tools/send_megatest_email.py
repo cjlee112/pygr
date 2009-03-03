3042995f20c954158f8cac854e5b1eb23a287d4a
Marek Szuba <mareks@leelab2.(none)>
2009-03-02 19:10:17 -0800

    Adapted the megatest runner and its companion send-mail script for recent portability updates, merged them into the tree.

---
+++ tests/tools/send_megatest_email.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import ConfigParser, os, smtplib, time
+from email.mime.text import MIMEText
+
+config = ConfigParser.ConfigParser({'mailServer' : ''})
+config.read([ os.path.expanduser('~/.pygrrc'), os.path.expanduser('~/pygr.cfg'), 'pygrrc', 'pygr.cfg' ])	# FIXME: make this OS-dependent?
+logdir = config.get('megatests', 'logDir')
+mailsender = config.get('megatests', 'mailFrom')
+mailserver = config.get('megatests', 'mailServer')
+maillist_fail = config.get('megatests', 'mailTo_failed')
+maillist_pass = config.get('megatests', 'mailTo_ok')
+
+timeStr = time.ctime()
+dateStr = ' '.join([ix for ix in timeStr.split(' ') if ':' not in ix])
+
+# Gather the runner script's output
+os.chdir(logdir)
+sendStr = 'MEGATEST report, generated ' + timeStr + '\n\n'
+sendStr += 'Test started: ' + open('tmp1_megatest.log', 'r').read()
+sendStr += 'PYTHONPATH = ' + open('tmp3_megatest.log', 'r').read() + '\n'
+sendStr += 'Output of standard tests:\n' + open('tmp2_megatest.log', 'r').read() + '\n\n'
+sendStr += 'Output of megatests:\n' + open('tmp4_megatest.log', 'r').read() + '\n\n'
+sendStr += 'Test finished: ' + open('tmp5_megatest.log', 'r').read()
+
+# Try to determine whether the test has failed or not
+nError = 0
+abnormalStop = 0
+for lines in sendStr.splitlines():
+    if lines[:6] == 'FINAL:':
+        nError += int(lines[7:30].split(' ')[0])
+        abnormalStop = 1
+if nError == 0 and abnormalStop:
+    maillist = maillist_pass
+else:
+    maillist = maillist_fail
+
+# Create and send the message
+msg = MIMEText(sendStr)
+msg['From'] = mailsender
+msg['To'] = maillist
+msg['Subject'] = 'Megatest on ' + dateStr + ' with ' + str(nError) + ' Errors'
+s = smtplib.SMTP(mailserver)
+s.connect()
+s.sendmail(mailsender, maillist.replace(',', ' ').split(), msg.as_string())
+s.close()
+
