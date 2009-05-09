a808756b669d67c35527aec0fc1eb25cbb72e3f2
Christopher Lee <leec@chem.ucla.edu>
2009-05-08 20:18:14 -0700

    added pygr.dir() to give Python 2.6+ style support for __dir__() in earlier versions of Python

--- pygr/__init__.py
+++ pygr/__init__.py
@@ -1,4 +1,4 @@
-
+import sys
 __version__ = "0.8.0"
 
 try:
@@ -8,4 +8,16 @@ except NameError:
     mdb = metabase.MetabaseList() # use default WORLDBASEPATH
     worldbase = mdb.Data
 
-__all__ = ('__version__', 'worldbase')
+if sys.version_info < (2, 6):
+    def dir(obj):
+        """provide Python 2.6+ style __dir__ support """
+        try:
+            get_dir = obj.__dir__
+        except AttributeError:
+            return __builtins__['dir'](obj)
+        else:
+            return get_dir()
+else: # Python 2.6 or later, just use the builtin dir()
+    dir = __builtins__['dir']
+    
+__all__ = ('__version__', 'worldbase', 'dir')
