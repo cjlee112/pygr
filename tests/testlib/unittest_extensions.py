07fd0d4314aff87fc6e3c3da52a191881061fa9e
Marek Szuba <Marek.Szuba@ucla.edu>
2009-11-05 19:39:46 -0800

    Addressed PEP-8 incompatibilities detected by pep8.py (Issue 117)

--- tests/testlib/unittest_extensions.py
+++ tests/testlib/unittest_extensions.py
@@ -5,13 +5,17 @@ Provide support for test skipping.
 import unittest
 from pygr import logger
 
+
 try:
     from nose.plugins.skip import SkipTest
 except ImportError: # no nose?
+
     class SkipTest(Exception):
         pass
 
+
 class PygrTestResult(unittest._TextTestResult):
+
     def __init__(self, *args, **kwargs):
         unittest._TextTestResult.__init__(self, *args, **kwargs)
         self.skipped = []
@@ -21,8 +25,7 @@ class PygrTestResult(unittest._TextTestResult):
         show_dots = kwargs.get('show_dots', 1)
         if verbosity == 0 and show_dots:
             self.dots = 1
-        
-        
+
     def addError(self, test, err):
         exc_type, val, _ = err
         if issubclass(exc_type, SkipTest):
@@ -34,15 +37,19 @@ class PygrTestResult(unittest._TextTestResult):
         else:
             unittest._TextTestResult.addError(self, test, err)
 
+
 class PygrTestRunner(unittest.TextTestRunner):
     """
     Support running tests that understand SkipTest.
     """
+
     def _makeResult(self):
         return PygrTestResult(self.stream, self.descriptions,
                               self.verbosity)
 
+
 class PygrTestProgram(unittest.TestProgram):
+
     def __init__(self, **kwargs):
         verbosity = kwargs.pop('verbosity', 1)
         if verbosity < 1:
