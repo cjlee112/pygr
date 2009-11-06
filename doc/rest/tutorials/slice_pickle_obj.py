cd429810747aa97a134ccffc19ade0468d2979fa
Marek Szuba <Marek.Szuba@ucla.edu>
2009-11-05 19:41:53 -0800

    Addressed PEP-8 incompatibilities detected by pep8.py (Issue 117)

--- doc/rest/tutorials/slice_pickle_obj.py
+++ doc/rest/tutorials/slice_pickle_obj.py
@@ -1,11 +1,14 @@
 class MySliceInfo(object):
+
     def __init__(self, id, start, stop, orientation):
         self.id = id
         self.start = start
         self.stop = stop
         self.orientation = orientation
 
+
 class MyFunkySliceInfo(object):
+
     def __init__(self, seq_id, begin, end, strand):
         self.seq_id = seq_id
         self.begin = begin
