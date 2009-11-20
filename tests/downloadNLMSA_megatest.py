da95629baa804c133df5e6e9b69c3c253988a3b0
Marek Szuba <Marek.Szuba@ucla.edu>
2009-11-19 18:44:50 -0800

    A major rewrite of downloadNLMSA_megatest: use a loopback HTTP server to avoid network problems, use local metabases instead of fooling around with worldbase, store the downloaded file with other temporary megatest files.

--- tests/downloadNLMSA_megatest.py
+++ tests/downloadNLMSA_megatest.py
@@ -1,57 +1,98 @@
+import ConfigParser
+import os.path
+import shutil
+import tempfile
+import threading
+import time
 import unittest
-from testlib import testutil, PygrTestProgram
 
-from pygr import nlmsa_utils
-import pygr.Data
-import os, tempfile, time
+from pygr import metabase
+from pygr.downloader import SourceURL
+from pygr.nlmsa_utils import NLMSABuilder
+from testlib import megatest_utils, testutil, PygrTestProgram
 
-def rm_recursive(top):
-    'recursively remove top and everything in it!'
-    for root, dirs, files in os.walk(top, topdown=False):
-        for name in files:
-            os.remove(os.path.join(root, name))
-        for name in dirs:
-            os.rmdir(os.path.join(root, name))
-    os.rmdir(top)
 
-#'http://biodb.bioinformatics.ucla.edu/PYGRDATA/dm2_multiz9way.txt.gz',
+def create_downloadable_resource(url, mdb, name, doc):
+    dfile = SourceURL(url)
+    nbuilder = NLMSABuilder(dfile)
+    nbuilder.__doc__ = doc
+    mdb.add_resource(name, nbuilder)
+    mdb.commit()
+
+
 class NLMSADownload_Test(unittest.TestCase):
-    '''try to save and build via download catalog auto-constructed from biodb site'''
-    def setUp(self, url='http://biodb.bioinformatics.ucla.edu/PYGRDATA/',
-                 testDir = tempfile.gettempdir()):
-        self.url = url
-        import random
-        self.testDir = os.path.join(testDir,'test%d' % random.randint(1,99999))
-        self.pygrdatapath = ','.join([self.testDir,
-                               'http://biodb2.bioinformatics.ucla.edu:5000'])
-        'create pygr.Data entries for all NLMSAs on biodb/PYGRDATA site'
-        os.mkdir(self.testDir)
-        pygr.Data.update(self.pygrdatapath) # set our desired path
-        from pygr.apps.catalog_downloads import save_NLMSA_downloaders
-        save_NLMSA_downloaders(self.url)
+    '''Try to download and build a relatively large alignment'''
+
+    def setUp(self):
+        config = ConfigParser.ConfigParser({'testOutputBaseDir': '.',
+                                            'httpdPort': 28145})
+        config.read([os.path.join(os.path.expanduser('~'), '.pygrrc'),
+                     os.path.join(os.path.expanduser('~'), 'pygr.cfg'),
+                     '.pygrrc', 'pygr.cfg'])
+        httpdPort = config.get('megatests_download', 'httpdPort')
+        httpdServedFile = config.get('megatests_download', 'httpdServedFile')
+        testOutputBaseDir = config.get('megatests', 'testOutputBaseDir')
+
+        self.resource_name = 'Test.NLMSA'
+
+        server_addr = ('127.0.0.1', httpdPort) # FIXME: randomise the port?
+        self.httpd = megatest_utils.HTTPServerLauncher(server_addr,
+                                                       httpdServedFile)
+        server_thread = threading.Thread(target=self.httpd.run)
+        server_thread.setDaemon(True)
+        server_thread.start()
+
+        self.test_dir = tempfile.mkdtemp(dir=testOutputBaseDir,
+                                         prefix='megatest')
+        dl_dir = os.path.join(self.test_dir, 'dl')
+        os.mkdir(dl_dir)
+        if 'WORLDBASEDOWNLOAD' in os.environ:
+            self.old_download = os.environ['WORLDBASEDOWNLOAD']
+        else:
+            self.old_download = None
+        os.environ['WORLDBASEDOWNLOAD'] = self.test_dir
+        if 'WORLDBASEBUILDDIR' in os.environ:
+            self.old_builddir = os.environ['WORLDBASEBUILDDIR']
+        else:
+            self.old_builddir = None
+        os.environ['WORLDBASEBUILDDIR'] = self.test_dir
+
+        self.mdb = metabase.MetabaseList(self.test_dir)
+        self.mdb_dl = metabase.MetabaseList(dl_dir)
+        url = 'http://%s:%d/' % server_addr + os.path.basename(httpdServedFile)
+        create_downloadable_resource(url, self.mdb_dl, self.resource_name,
+                                     'An example downloadable NLMSA')
+
     def test_download(self):
         'Test downloading NLMSA data'
-        os.environ['PYGRDATADOWNLOAD'] = self.testDir
-        os.environ['PYGRDATABUILDDIR'] = self.testDir
         t = time.time()
-        pygr.Data.Bio.MSA.UCSC.dm2_multiz9way() # build it!
-        t1 = time.time() - t # 1st build time
-        pygr.Data.clear_cache() # reload rsrc db
+        msa_dl = self.mdb_dl(self.resource_name)  # download and build it!
+        t1 = time.time() - t
         t = time.time()
-        msa = pygr.Data.Bio.MSA.UCSC.dm2_multiz9way() # already built
-        t2 = time.time() - t # 2nd request time
+        self.mdb.add_resource(self.resource_name, msa_dl)
+        self.mdb.commit()
+        del msa_dl
+        msa = self.mdb(self.resource_name)  # already built
+        t2 = time.time() - t
         assert t2 < t1/3., 'second request took too long!'
         chr4 = msa.seqDict['dm2.chr4']
         result = msa[chr4[:10000]]
         assert len(result) == 9
+
     def tearDown(self):
-        'clean up our temporary directory, restore pygr.Data path'
-        rm_recursive(self.testDir)
-        # Restore original pygr.Data path to remedy lack of isolation
-        # between tests from the same run
-        pygr.Data.update(None)
+        # Just in case - the server thread is daemonic so it will get
+        # terminated when the main one finishes.
+        self.httpd.request_shutdown()
+        if self.old_download is not None:
+            os.environ['WORLDBASEDOWNLOAD'] = self.old_download
+        else:
+            del os.environ['WORLDBASEDOWNLOAD']
+        if self.old_builddir is not None:
+            os.environ['WORLDBASEBUILDDIR'] = self.old_builddir
+        else:
+            del os.environ['WORLDBASEBUILDDIR']
+        shutil.rmtree(self.test_dir)
 
 
 if __name__ == '__main__':
     PygrTestProgram(verbosity=2)
-
