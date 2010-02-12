d4af6e731f4fb1a426a4ceeb7eb69534add41317
Marek Szuba <Marek.Szuba@ucla.edu>
2010-02-11 18:17:12 -0800

    Come to think of it, having the identifiers in variables is kind of pointless - at least the way the code looks now. Reverted this change.

--- tests/apps_ucscensembl_test.py
+++ tests/apps_ucscensembl_test.py
@@ -19,34 +19,29 @@ class UCSCEnsembl_Test(unittest.TestCase):
                          badname)
 
     def test_transcriptdb(self):
-        trans_id = 'ENST00000000233'
         trans_db = self.iface.trans_db
-        mrna = trans_db[trans_id]
+        mrna = trans_db['ENST00000000233']
         self.assertEqual(repr(mrna), 'annotENST00000000233[0:3295]')
         self.assertEqual(repr(mrna.sequence), 'chr7[127015694:127018989]')
         self.assertEqual(repr(mrna.mrna_sequence), 'ENST00000000233[0:1037]')
 
     def test_genedb(self):
-        gene_id = 'ENSG00000000003'
-        gene_id_multitrans = 'ENSG00000168958'
         gene_db = self.iface.gene_db
-        gene = gene_db[gene_id]
+        gene = gene_db['ENSG00000000003']
         self.assertEqual(repr(gene), 'annotENSG00000000003[0:8000]')
         self.assertEqual(repr(gene.sequence), '-chrX[99770450:99778450]')
-        self.assertEqual(repr(gene_db[gene_id_multitrans]),
+        self.assertEqual(repr(gene_db['ENSG00000168958']),
                          'annotENSG00000168958[0:32595]')
 
     def test_proteindb(self):
-        prot_id = 'ENSP00000372525'
         prot_db = self.iface.prot_db
-        prot = prot_db[prot_id]
+        prot = prot_db['ENSP00000372525']
         self.assertEqual(repr(prot), 'ENSP00000372525')
         self.assertEqual(repr(prot.sequence), 'ENSP00000372525[0:801]')
 
     def test_exondb(self):
-        exon_id = 'ENSE00000720378'
         exon_db = self.iface.exon_db
-        exon = exon_db[exon_id]
+        exon = exon_db['ENSE00000720378']
         self.assertEqual(repr(exon), 'annotENSE00000720378[0:110]')
         self.assertEqual(repr(exon.sequence), 'chr7[127016774:127016884]')
         self.assertEqual(len(exon_db), 297956)
