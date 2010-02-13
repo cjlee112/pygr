3b3c450a27219403c3d8c1a8c2c6376e19f1fb4d
Marek Szuba <Marek.Szuba@ucla.edu>
2010-02-12 17:06:35 -0800

    Added a test of maps between databases.

--- tests/apps_ucscensembl_test.py
+++ tests/apps_ucscensembl_test.py
@@ -60,6 +60,34 @@ class UCSCEnsembl_Test(unittest.TestCase):
         self.assertEqual(snp.refUCSC, 'G')
         self.assertEqual(snp.observed, 'A/G')
 
+    def test_maps(self):
+        'Test mapping between different databases'
+        mrna = self.iface.trans_db['ENST00000000233']
+        gene = self.iface.gene_db['ENSG00000168958']
+        prot = self.iface.prot_db['ENSP00000372525']
+        exon = self.iface.exon_db['ENSE00000720378']
+        trans_of_prot = self.iface.protein_transcript_id_map[prot]
+        self.assertEqual(repr(trans_of_prot.id), "'ENST00000383052'")
+        prot_of_mrna = (~self.iface.protein_transcript_id_map)[mrna]
+        self.assertEqual(repr(prot_of_mrna.id), "'ENSP00000000233'")
+        trans_of_gene = self.iface.transcripts_in_genes_map[gene].keys()
+        self.assertEqual(repr(trans_of_gene),
+                         '''[annotENST00000353339[0:32595], \
+annotENST00000409565[0:32541], annotENST00000409616[0:31890], \
+annotENST00000354503[0:32560], annotENST00000349901[0:32560], \
+annotENST00000337110[0:32560], annotENST00000304593[0:32560], \
+annotENST00000392059[0:30316], annotENST00000392058[0:28082]]''')
+        gene_of_mrna = (~self.iface.transcripts_in_genes_map)[mrna].keys()
+        self.assertEqual(repr(gene_of_mrna), '[annotENSG00000004059[0:3295]]')
+        trans_of_exon = self.iface.ens_transcripts_of_exons_map[exon].keys()
+        self.assertEqual(repr(trans_of_exon), '[annotENST00000000233[0:3295]]')
+        exons_of_mrna = self.iface.ens_exons_in_transcripts_map[mrna].keys()
+        self.assertEqual(repr(exons_of_mrna),
+                         '''[annotENSE00001123404[0:161], \
+annotENSE00000720374[0:81], annotENSE00000720378[0:110], \
+annotENSE00000720381[0:72], annotENSE00000720384[0:126], \
+annotENSE00000882271[0:487]]''')
+
 
 if __name__ == '__main__':
     PygrTestProgram(verbosity=2)
